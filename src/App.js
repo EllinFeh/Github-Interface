8a2fcd48dc13187857c0bccfb59e64e02365b147
EllinFeh <elisonfelipe16@hotmail.com>
2023-09-16 12:21:27 -0300

    first commit project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Children } from "react";
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+
+      <Layout>
+
+        <div className="user">
+          <img src="https://avatars.githubusercontent.com/u/1?v=4" alt="avataruser" />
+          <h1>Elison Felipe</h1>
+          <h3>Username</h3>
+          <span>felipe</span>
+
+          <div>
+            <h4>Followers</h4>
+            <span>154</span>
+          </div>
+
+          <div>
+            <h4>Starreds</h4>
+            <span>21</span>
+          </div>
+
+          <div>
+            <h4>Followings</h4>
+            <span>8</span>
+          </div>
+
+        </div>
+
+        <div className="repositories"></div>
+        <div className="starreds"></div>
+
+      </Layout>
+      
+    </main>
   );
 }
 
---
+++ src/components/layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Layout({children}) {
+  return (
+    <section>
+        <header>
+        </header>
+        {children}
+    </section>
+  )
+}
+
+export default Layout

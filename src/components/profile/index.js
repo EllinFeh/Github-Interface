22e12c3bb5d585c53e6258a3762cf461a220b0c0
EllinFeh <elisonfelipe16@hotmail.com>
2023-09-16 15:27:41 -0300

    third commit

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -5,45 +5,45 @@ const Profile = () => {
     return (
 
         <div>
-        
-            <S.WrapperBackground>
-            <S.Wrapper>
+            <S.WrapperDad>
+                <S.WrapperBackground>
+                    <S.Wrapper>
 
-                <S.WrapperAvatar src="https://avatars.githubusercontent.com/u/88490490?v=4" alt="avataruser" />
+                        <S.WrapperAvatar src="https://avatars.githubusercontent.com/u/88490490?v=4" alt="avataruser" />
 
-
-                <div>
-                    <h1>Elison Felipe</h1>
-                    <S.WrapperUsername>
-                        <h3>Username:</h3>
-                        <span>EllinFeh</span>
-                    </S.WrapperUsername>
-
-
-                    <S.WrapperStatusCount>
-                        <div>
-                            <h4>Followers</h4>
-                            <span>154</span>
-                        </div>
 
                         <div>
-                            <h4>Starreds</h4>
-                            <span>21</span>
+                            <h1>Elison Felipe</h1>
+                            <S.WrapperUsername>
+                                <h3>Username:</h3>
+                                <span>EllinFeh</span>
+                            </S.WrapperUsername>
+
+
+                            <S.WrapperStatusCount>
+                                <div>
+                                    <h4>Followers</h4>
+                                    <span>154</span>
+                                </div>
+
+                                <div>
+                                    <h4>Starreds</h4>
+                                    <span>21</span>
+                                </div>
+
+                                <div>
+                                    <h4>Followings</h4>
+                                    <span>8</span>
+                                </div>
+                            </S.WrapperStatusCount>
+
                         </div>
 
-                        <div>
-                            <h4>Followings</h4>
-                            <span>8</span>
-                        </div>
-                    </S.WrapperStatusCount>
 
-                </div>
+                    </S.Wrapper >
+                </S.WrapperBackground>
+            </S.WrapperDad>
 
-
-            </S.Wrapper >
-            </S.WrapperBackground>
-
-         
         </div>
     )
 }
--- src/components/profile/styled.js
+++ src/components/profile/styled.js
@@ -19,6 +19,7 @@ align-items: center;
 
 export const WrapperUsername = styled.div`
 display: flex;
+grid-gap: 5px;
 align-items: center;
 `;
 
@@ -26,14 +27,24 @@ export const WrapperAvatar = styled.img`
 border-radius: 50%;
 width: 150px;
 height: 150px;
-margin: 10px;
 `;
 
 export const WrapperBackground = styled.div`
-position:fixed;
-width: 100vw;
-height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
 background-color: #303030;
 border-radius: 40px;
-
+max-height: 250px;
+max-width: 500px;
+width: 100vw;
+height: 100vh;
 `;
+
+export const WrapperDad = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+
